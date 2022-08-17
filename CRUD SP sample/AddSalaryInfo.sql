9685a311370e37fe5b6d5f980432b2bcfee3e366
hediye jamili <61234450+hedieh-hj@users.noreply.github.com>
2022-08-17 12:29:14 +0500

    Update AddSalaryInfo.sql

--- CRUD SP sample/AddSalaryInfo.sql
+++ CRUD SP sample/AddSalaryInfo.sql
@@ -2,14 +2,14 @@
 	DROP PROCEDURE [salaryorash].[AddSalaryInfo]
 GO
 CREATE PROCEDURE [salaryorash].[AddSalaryInfo] 
-	@RowNo						BIGINT ,
+	@RowNo					BIGINT ,
 	@TafsiliCode				BIGINT ,
-	@FirstName					NVARCHAR(150),
-	@LastName					NVARCHAR(150),
+	@FirstName				NVARCHAR(150),
+	@LastName				NVARCHAR(150),
 	@NationalCode				NVARCHAR(20),
 	@InsuranceName				NVARCHAR(250),
 	@InsuranceCode				NVARCHAR(250),
-	@TimeAttendancePersonalCode	NVARCHAR(250),
+	@TimeAttendancePersonalCode		NVARCHAR(250),
 	@JobPosition				NVARCHAR(250),
 	@WorkshopCode				NVARCHAR(250),
 	@WorkshopName				NVARCHAR(250),
@@ -17,31 +17,31 @@ CREATE PROCEDURE [salaryorash].[AddSalaryInfo]
 	@PersonalGroupName			NVARCHAR(250),
 	@TafsiliCode2				BIGINT,
 	@TafsiliName2				NVARCHAR(250),
-	@AddTitle					NVARCHAR(250),
-	@AddAmount					DECIMAL(18,8),
-	@AddPrice					DECIMAL(18,8),
-	@MinTitle					NVARCHAR(250),
-	@MinAmount					DECIMAL(18,8),
-	@MinPrice					DECIMAL(18,8),
+	@AddTitle				NVARCHAR(250),
+	@AddAmount				DECIMAL(18,8),
+	@AddPrice				DECIMAL(18,8),
+	@MinTitle				NVARCHAR(250),
+	@MinAmount				DECIMAL(18,8),
+	@MinPrice				DECIMAL(18,8),
 	@SumMinPrice				DECIMAL(18,8),
-	@Year						INT,
-	@Month						INT	
+	@Year					INT,
+	@Month					INT	
 			
 WITH ENCRYPTION
 AS
 BEGIN
 	BEGIN TRY
 		INSERT INTO [salaryorash].[SalaryInfo]
-					(	RowNo , TafsiliCode , FirstName ,	LastName , NationalCode	, InsuranceName , InsuranceCode , TimeAttendancePersonalCode , JobPosition	, WorkshopCode , WorkshopName ,
-						PersonalGroupCode , PersonalGroupName ,TafsiliCode2 , TafsiliName2 , AddTitle, AddAmount , AddPrice , MinTitle , MinAmount , MinPrice , SumMinPrice , Year , Month)
+					(RowNo , TafsiliCode , FirstName ,	LastName , NationalCode	, InsuranceName , InsuranceCode , TimeAttendancePersonalCode , JobPosition	, WorkshopCode , WorkshopName ,
+					PersonalGroupCode , PersonalGroupName ,TafsiliCode2 , TafsiliName2 , AddTitle, AddAmount , AddPrice , MinTitle , MinAmount , MinPrice , SumMinPrice , Year , Month)
 		
-		VALUES (	@RowNo , @TafsiliCode ,	@FirstName , @LastName , @NationalCode , @InsuranceName , @InsuranceCode , @TimeAttendancePersonalCode	, @JobPosition , @WorkshopCode , @WorkshopName ,
-					@PersonalGroupCode , @PersonalGroupName , @TafsiliCode2 , @TafsiliName2 , @AddTitle ,@AddAmount , @AddPrice , @MinTitle , @MinAmount , @MinPrice , @SumMinPrice , @Year , @Month)
+		VALUES (@RowNo , @TafsiliCode ,	@FirstName , @LastName , @NationalCode , @InsuranceName , @InsuranceCode , @TimeAttendancePersonalCode	, @JobPosition , @WorkshopCode , @WorkshopName ,
+			@PersonalGroupCode , @PersonalGroupName , @TafsiliCode2 , @TafsiliName2 , @AddTitle ,@AddAmount , @AddPrice , @MinTitle , @MinAmount , @MinPrice , @SumMinPrice , @Year , @Month)
 	END TRY
 	BEGIN CATCH
 		ROLLBACK
 		DECLARE @ErrorMessage	NVARCHAR(4000)	,
-				@ErrorSeverity  INT				,
+				@ErrorSeverity  INT,
 				@ErrorState     INT
 		SELECT 
 			@ErrorMessage  = CAST(ERROR_NUMBER() As VARCHAR(10)) + '$' +ERROR_MESSAGE(),
