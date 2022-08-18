a467ba9f39649dbe928a30ca0973e767ef414a8d
hedieh-hj <hediyejamili79@gmail.com>
2022-08-18 10:47:54 +0500

    updated sp

---
+++ CRUD SP sample/GetSalaryInfo.sql
@@ -0,0 +1,48 @@
+﻿IF OBJECT_ID ('salaryorash.GetSalaryInfo') IS NOT NULL  
+	DROP PROCEDURE [salaryorash].[GetSalaryInfo]
+GO
+CREATE PROCEDURE [salaryorash].[GetSalaryInfo] 
+	@NationalCode NVARCHAR(20) ,  ---> کد ملی
+	@Year	      INT	       ,  ---> سال
+	@Month	      INT             ---> ماه
+WITH ENCRYPTION
+AS 
+BEGIN 
+	BEGIN
+		SELECT  1 AS Type   ,
+				ROW_NUMBER() OVER(PARTITION BY [NationalCode] , [Year] , [Month] ORDER BY [NationalCode] ASC) RowID ,
+				AddTitle    ,
+				AddAmount   ,
+				AddPrice    ,
+				MinTitle    ,
+				MinAmount   ,
+				MinPrice    ,
+				SumMinPrice ,
+				NULL TotalPureSalaryValue 
+
+		FROM [salaryorash].[SalaryInfo]
+		WHERE   NationalCode = @NationalCode		
+			AND [Year]       = @Year       
+			AND [Month]      = @Month
+		----------------------------------------------------
+		UNION ALL ------------------------------------------
+		----------------------------------------------------
+		SELECT  2 AS                           Type        ,
+				NULL                           RowID       ,
+				NULL                           AddTitle    ,
+				NULL                           AddAmount   ,
+				SUM(AddPrice)                  AddPrice    ,
+				NULL                           MinTitle    ,
+				NULL                           MinAmount   ,
+				SUM(MinPrice)+SUM(SumMinPrice) MinPrice    ,
+				NULL SumMinPrice						   ,
+				SUM(AddPrice)-(SUM(MinPrice)+SUM(SumMinPrice)) TotalPureSalaryValue
+
+		FROM [salaryorash].[SalaryInfo]
+		WHERE   NationalCode = @NationalCode		
+			AND [Year]       = @Year       
+			AND [Month]      = @Month
+		GROUP BY NationalCode , [Year] , [Month]
+	
+	END
+END
---
+++ CRUD SP sample/GetUserInfo.sql
@@ -0,0 +1,20 @@
+﻿IF OBJECT_ID ('salaryorash.GetUserInfo') IS NOT NULL  
+	DROP PROCEDURE [salaryorash].[GetUserInfo]
+GO
+CREATE PROCEDURE [salaryorash].[GetUserInfo] 
+	@NationalCode NVARCHAR(20)   ---> کد ملی
+WITH ENCRYPTION
+AS 
+BEGIN 
+	SELECT  [FirstName    ] ,
+			[LastName     ] ,
+			[NationalCode ] ,
+			[WorkshopName ] ,
+			[WorkshopCode ] ,
+			[InsuranceName]	,
+			[InsuranceCode]	,
+			[JobPosition  ]	
+
+		FROM [salaryorash].[SalaryInfo]
+		WHERE   NationalCode = @NationalCode
+END
